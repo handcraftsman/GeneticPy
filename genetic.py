754e695cf29b255a3642fb36b02b7c21b6f55b48
Clinton <sheppard@cs.unm.edu>
2015-06-09 14:13:59 -0500

    introduce Individual

--- genetic.py
+++ genetic.py
@@ -1,31 +1,39 @@
 ﻿import random
 
-def mutate(parent, geneSet):
+def mutate(parent, geneSet, get_fitness):
    geneIndex = random.randint(0, len(geneSet) -1);
-   index = random.randint(0, len(parent) - 1)
-   genes = list(parent)
+   index = random.randint(0, len(parent.Genes) - 1)
+   genes = list(parent.Genes)
    genes[index] = geneSet[geneIndex]
-   return(''.join(genes))
+   childGenes = (''.join(genes))
+   fitness = get_fitness(childGenes)
+   return Individual(childGenes, fitness)
 
-def generateParent(length, geneSet):
+def generateParent(length, geneSet, get_fitness):
     genes = list("")
     for i in range(0,length):
         geneIndex = random.randint(0, len(geneSet) -1);
         genes.append(geneSet[geneIndex])
-    return(''.join(genes))
+    childGenes = (''.join(genes))
+    fitness = get_fitness(childGenes)
+    return Individual(childGenes,fitness)
 
 def getBest(get_fitness, display, targetLen, geneSet):
     random.seed()
-    bestParent = generateParent(targetLen, geneSet)
-    bestFitness = get_fitness(bestParent)
+    bestParent = generateParent(targetLen, geneSet, get_fitness)
     display(bestParent)
 
-    while bestFitness < len(bestParent):
-       child = mutate(bestParent, geneSet)
-       childFitness = get_fitness(child)
+    while bestParent.Fitness < targetLen:
+       child = mutate(bestParent, geneSet, get_fitness)
 
-       if childFitness > bestFitness:
-          bestFitness = childFitness
+       if child.Fitness > bestParent.Fitness:
           bestParent = child
           display(bestParent)
     return bestParent
+
+class Individual:
+   Genes = None
+   Fitness = None
+   def __init__(self, genes, fitness):
+        self.Genes = genes
+        self.Fitness = fitness
--- stringDuplicationTests.py
+++ stringDuplicationTests.py
@@ -5,14 +5,13 @@ import genetic
 def getFitness(candidate, target):
    fitness = 0
    for i in range(0, len(candidate)):
-       if target[i] == candidate[i]:
-           fitness += 1
+        if target[i] == candidate[i]:
+            fitness += 1
    return(fitness)
 
 def display(candidate, target, startTime):
     timeDiff = datetime.datetime.now() - startTime
-    fitness = getFitness(candidate, target)
-    print ("%s\t%i\t%s" % (candidate, fitness, str(timeDiff)))
+    print ("%s\t%i\t%s" % (candidate.Genes, candidate.Fitness, str(timeDiff)))
 
 class StringDuplicationTests(unittest.TestCase):
     def test(self):
@@ -22,7 +21,7 @@ class StringDuplicationTests(unittest.TestCase):
         fnDisplay = lambda candidate: display(candidate, target, startTime)
         fnGetFitness = lambda candidate: getFitness(candidate, target)
         best = genetic.getBest(fnGetFitness, fnDisplay, len(target), geneset)
-        self.assertEqual(best, target)
+        self.assertEqual(best.Genes, target)
 
 if __name__ == '__main__':
     unittest.main()
